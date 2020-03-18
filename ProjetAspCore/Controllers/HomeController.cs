99c456211ca750e06ec96e6fdea72ec4eae5a683
Cédric Hotz <36566958+cedricHOTZ@users.noreply.github.com>
2020-03-18 13:50:59 +0100

    cedric
    Mise en place de la page d'accueil avec la liste des livres, les plus vendus

---
+++ ProjetAspCore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetAspCore.Repository;
+using ProjetAspCore.ViewsModels;
+
+namespace ProjetAspCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public HomeController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public IActionResult Index()
+        {
+            var listBook = new BookListViewModel();
+            listBook.Books = _bookRepository.GetAllBooks().Take(3);
+            return View(listBook);
+        }
+    }
+}
